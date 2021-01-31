e6394f70fc5d4e8083f7dab379aa3e23bb477507
Peter Munch <peterrmuench@gmail.com>
2021-01-31 10:48:59 +0100

    Add test to determine if point is within circle

---
+++ tests/sharp_interfaces_03.cc
@@ -0,0 +1,222 @@
+// --------------------------------------------------------------------------
+//
+// Copyright (C) 2021 by the adaflo authors
+//
+// This file is part of the adaflo library.
+//
+// The adaflo library is free software; you can use it, redistribute it,
+// and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.  The full text of the
+// license can be found in the file LICENSE at the top level of the adaflo
+// distribution.
+//
+// --------------------------------------------------------------------------
+
+#include <deal.II/base/mpi.h>
+
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/fe/fe_point_evaluation.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/mapping_fe_field.h>
+#include <deal.II/fe/mapping_q_generic.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/grid_tools_cache.h>
+
+#include <deal.II/matrix_free/fe_evaluation.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+
+#include <adaflo/level_set_okz_compute_curvature.h>
+#include <adaflo/level_set_okz_compute_normal.h>
+#include <adaflo/level_set_okz_preconditioner.h>
+#include <adaflo/level_set_okz_reinitialization.h>
+#include <adaflo/util.h>
+
+//#include <boost/geometry/domains/gis/io/wkt/wkt.hpp>
+
+using VectorType = LinearAlgebra::distributed::Vector<double>;
+
+namespace dealii
+{
+  namespace VectorTools
+  {
+    template <int dim, int spacedim, typename VectorType>
+    void
+    VectorTools::get_position_vector(const DoFHandler<dim, spacedim> &dof_handler_dim,
+                                     VectorType &                     euler_vector,
+                                     const Mapping<dim, spacedim> &   mapping)
+    {
+      FEValues<dim, spacedim> fe_eval(
+        mapping,
+        dof_handler_dim.get_fe(),
+        Quadrature<dim>(dof_handler_dim.get_fe().get_unit_support_points()),
+        update_quadrature_points);
+
+      Vector<double> temp;
+
+      for (const auto &cell : dof_handler_dim.active_cell_iterators())
+        {
+          fe_eval.reinit(cell);
+
+          temp.reinit(fe_eval.dofs_per_cell);
+
+          for (const auto q : fe_eval.quadrature_point_indices())
+            {
+              const auto point = fe_eval.quadrature_point(q);
+
+              const unsigned int comp =
+                dof_handler_dim.get_fe().system_to_component_index(q).first;
+
+              temp[q] = point[comp];
+            }
+
+          cell->set_dof_values(temp, euler_vector);
+        }
+    }
+  } // namespace VectorTools
+} // namespace dealii
+
+
+
+template <int dim>
+void
+test()
+{
+  //
+  const unsigned int spacedim       = dim + 1;
+  const unsigned int fe_degree      = 3;
+  const unsigned int mapping_degree = fe_degree;
+  const unsigned int n_refinements  = 5;
+
+  //
+  Triangulation<dim, spacedim> tria;
+  GridGenerator::hyper_sphere(tria, Point<spacedim>(), 0.5);
+  tria.refine_global(n_refinements);
+
+  FE_Q<dim, spacedim>       fe(fe_degree);
+  DoFHandler<dim, spacedim> dof_handler(tria);
+  dof_handler.distribute_dofs(fe);
+
+  FESystem<dim, spacedim>   fe_dim(fe, spacedim);
+  DoFHandler<dim, spacedim> dof_handler_dim(tria);
+  dof_handler_dim.distribute_dofs(fe_dim);
+
+  //
+  Vector<double> euler_vector(dof_handler_dim.n_dofs());
+  VectorTools::get_position_vector(dof_handler_dim,
+                                   euler_vector,
+                                   MappingQGeneric<dim, spacedim>(mapping_degree));
+  MappingFEField<dim, spacedim> mapping(dof_handler_dim, euler_vector);
+
+  //
+  typedef boost::geometry::model::d2::point_xy<double> point_type;
+  typedef boost::geometry::model::polygon<point_type>  polygon_type;
+
+
+
+  std::vector<boost::geometry::model::d2::point_xy<double>> points;
+  {
+    FEValues<dim, spacedim> fe_eval(mapping,
+                                    fe,
+                                    QGauss<dim>(fe_degree + 1),
+                                    update_quadrature_points);
+
+    for (const auto &cell : dof_handler.active_cell_iterators())
+      {
+        fe_eval.reinit(cell);
+
+        for (const auto q : fe_eval.quadrature_point_indices())
+          {
+            const auto point = fe_eval.quadrature_point(q);
+            points.emplace_back(point[0], point[1]);
+          }
+      }
+  }
+
+  polygon_type poly;
+  boost::geometry::assign_points(poly, points);
+
+  const unsigned int background_n_global_refinements = 6;
+  const unsigned int background_fe_degree            = 1;
+
+  Triangulation<spacedim> background_tria;
+  GridGenerator::hyper_cube(background_tria, -1.0, +1.0);
+  background_tria.refine_global(background_n_global_refinements);
+
+  FE_Q<spacedim>       background_fe(background_fe_degree);
+  DoFHandler<spacedim> background_dof_handler(background_tria);
+  background_dof_handler.distribute_dofs(background_fe);
+
+  MappingQ1<spacedim> background_mapping;
+
+  VectorType force_vector_sharp_interface(background_dof_handler.n_dofs());
+  {
+    FEValues<spacedim> fe_eval(background_mapping,
+                               background_fe,
+                               background_fe.get_unit_support_points(),
+                               update_quadrature_points);
+
+    for (const auto &cell : background_dof_handler.active_cell_iterators())
+      {
+        fe_eval.reinit(cell);
+
+        Vector<double> vec(fe_eval.n_quadrature_points);
+
+        for (const auto q : fe_eval.quadrature_point_indices())
+          {
+            const auto point = fe_eval.quadrature_point(q);
+            point_type p(point[0], point[1]);
+            vec[q] = static_cast<double>(boost::geometry::within(p, poly));
+          }
+
+        cell->set_dof_values(vec, force_vector_sharp_interface);
+      }
+  }
+
+  {
+    DataOutBase::VtkFlags flags;
+    flags.write_higher_order_cells = true;
+
+    DataOut<spacedim> data_out;
+    data_out.set_flags(flags);
+    data_out.attach_dof_handler(background_dof_handler);
+    data_out.add_data_vector(background_dof_handler,
+                             force_vector_sharp_interface,
+                             "force");
+
+    data_out.build_patches(background_mapping, background_fe_degree + 1);
+    data_out.write_vtu_with_pvtu_record("./",
+                                        "sharp_interface_02_background",
+                                        0,
+                                        MPI_COMM_WORLD);
+  }
+
+  /*
+  points.emplace_back(0,0);
+  points.emplace_back(1,0);
+  points.emplace_back(1,1);
+  points.emplace_back(0,1);
+  points.emplace_back(0,0);
+   */
+
+
+  std::cout << boost::geometry::area(poly) << std::endl;
+}
+
+
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi(argc, argv, 1);
+
+  test<1>();
+}
